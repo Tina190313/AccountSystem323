4502ab620efd9e1d44a4b10c2a0c3a7112a70fe8
Tina990313 <tina.vanniekerk99@gmail.com>
2021-10-08 20:16:25 +0100

    Database entities created, Respository setup

---
+++ AccountSystemDomain/src/main/java/za/ac/nwu/ac/domain/Demo.java
@@ -0,0 +1,8 @@
+package za.ac.nwu.ac.domain;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Demo {
+
+}
--- AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
+++ AccountSystemWebSpringBoot/src/main/java/za/ac/nwu/ac/web/sb/controller/AccountTypeController.java
@@ -1,33 +1,32 @@
 package za.ac.nwu.ac.web.sb.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
-import za.ac.nwu.ac.domain.dto.service.GeneralResponse;
+import za.ac.nwu.ac.domain.service.GeneralResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 @RestController
-@RequestMapping("account")
+@RequestMapping("account-type")
 
 public class AccountTypeController {
     @GetMapping("/all")
-    @ApiOperation(value = "Echo the Ping.", notes = "This echo the ping back to the client")
-            @ApiResponses(value =
-                    {@ApiResponse(code = 200, message = "The Ping was received and echoed", response = GeneralResponse.class),
-            @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
-    @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
-    @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
+    @ApiOperation(value = "Gets all configured Account Types.", notes = "Returns a list of Account types")
+    @ApiResponses(value =
+            {@ApiResponse(code = 200, message = "Returned", response = GeneralResponse.class),
+                    @ApiResponse(code = 400, message = "Bad Request", response = GeneralResponse.class),
+                    @ApiResponse(code = 404, message = "Not found", response = GeneralResponse.class),
+                    @ApiResponse(code = 500, message = "Internal Server Error", response = GeneralResponse.class)})
 
-    public ResponseEntity<String> ping(@RequestParam(value = "value that will be echoed", defaultValue = "pong") String echo)
-    {
-        return new ResponseEntity<>(echo, HttpStatus.OK);
+    public ResponseEntity<GeneralResponse<String>> getAll() {
+        GeneralResponse<String> response = new GeneralResponse<>(true, "No types found");
+        {
+            return new ResponseEntity<>(response, HttpStatus.OK);
         }
 
-
+    }
 }
